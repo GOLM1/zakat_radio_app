e3a8620aadbb0f79c4ff2b9beaa089572d2f8045
0zu5 <abdo42821@gmail.com>
2026-05-01 01:20:21 +0200

    Prepare app for store release

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,16 +1,11 @@
-import 'package:flutter/material.dart';
+import 'dart:io';
+
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:zakat_radio_app/main.dart';
-
 void main() {
-  testWidgets('radio home shows modern playback UI', (tester) async {
-    await tester.pumpWidget(const MyApp());
-
-    expect(find.text('إذاعة\nصندوق الزكاة الليبي'), findsOneWidget);
-    expect(find.text('البث المباشر'), findsOneWidget);
-    expect(find.text('تشغيل البث'), findsOneWidget);
-    expect(find.byIcon(Icons.play_arrow_rounded), findsOneWidget);
-    expect(find.byType(Image), findsNWidgets(2));
+  test('release assets exist', () {
+    expect(File('assets/images/logo.png').existsSync(), isTrue);
+    expect(File('assets/images/wasl.png').existsSync(), isTrue);
+    expect(File('assets/images/now_playing.png').existsSync(), isTrue);
   });
 }
